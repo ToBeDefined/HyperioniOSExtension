a3b2fef5c720ab4c33b267d8c0563d3d400cba3b
TBD <weinanshao@163.com>
2018-04-19 16:01:54 +0800

    修改替换函数方法

--- HYPUIMainThreadChecker/UIView+HYPUIMainThreadChecker.m
+++ HYPUIMainThreadChecker/UIView+HYPUIMainThreadChecker.m
@@ -10,25 +10,52 @@
 #import "HYPUIMainThreadCheckerPluginModule.h"
 #import "UIView+HYPUIMainThreadChecker.h"
 
-static inline void __t_main_thread_checker_safe_swizzling_exchange_instance_method(Class cls, SEL originalSel, SEL swizzledSel) {
+#pragma mark - Safe Exchange Method & isMainQueue Check Func
+#pragma mark -
+
+static inline
+void __t_main_thread_checker_safe_swizzling_exchange_instance_method(Class cls, SEL originalSel, SEL swizzledSel) {
     Method originalMethod = class_getInstanceMethod(cls, originalSel);
     Method swizzledMethod = class_getInstanceMethod(cls, swizzledSel);
     
-    BOOL success = class_addMethod(cls,
-                                   originalSel,
-                                   method_getImplementation(swizzledMethod),
-                                   method_getTypeEncoding(swizzledMethod));
-    if (success) {
+    // 交换实现进行添加函数
+    BOOL addOriginSELSuccess = class_addMethod(cls,
+                                               originalSel,
+                                               method_getImplementation(swizzledMethod),
+                                               method_getTypeEncoding(swizzledMethod));
+    BOOL addSwizzlSELSuccess = class_addMethod(cls,
+                                               swizzledSel,
+                                               method_getImplementation(originalMethod),
+                                               method_getTypeEncoding(originalMethod));
+    // 全都添加成功，返回
+    if (addOriginSELSuccess && addSwizzlSELSuccess) {
+        return;
+    }
+    // 全都添加失败，已经添加过了方法，交换
+    if (!addOriginSELSuccess && !addSwizzlSELSuccess) {
+        method_exchangeImplementations(originalMethod,
+                                       swizzledMethod);
+        return;
+    }
+    // addOriginSELSuccess 成功，addSwizzlSELSuccess 失败，replace SwizzlSel
+    if (addOriginSELSuccess && !addSwizzlSELSuccess) {
         class_replaceMethod(cls,
                             swizzledSel,
                             method_getImplementation(originalMethod),
                             method_getTypeEncoding(originalMethod));
-    } else {
-        method_exchangeImplementations(originalMethod,
-                                       swizzledMethod);
+        return;
+    }
+    // addSwizzlSELSuccess 成功，addOriginSELSuccess 失败，replace originSEL
+    if (addOriginSELSuccess && !addSwizzlSELSuccess) {
+        class_replaceMethod(cls,
+                            originalSel,
+                            method_getImplementation(swizzledMethod),
+                            method_getTypeEncoding(swizzledMethod));
+        return;
     }
 }
 
+static inline
 BOOL isMainQueue(void) {
     static const void *mainQueueKey = @"__T_MainQueue_T__";
     static void *mainQueueContext = @"__T_MainQueue_T__";
@@ -39,6 +66,10 @@ BOOL isMainQueue(void) {
     
     return dispatch_get_specific(mainQueueKey) == mainQueueContext;
 }
+
+
+#pragma mark - Implementation Replace
+#pragma mark -
 
 @implementation UIView (HYPUIMainThreadChecker)
 
